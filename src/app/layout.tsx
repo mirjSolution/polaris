266faa435e6f815b77709be60f9a8a0885f63fca
mirjSolution <ricojohndatoon23@gmail.com>
2026-01-05 12:34:00 -0500

    02: Authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,18 @@
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Mono } from "next/font/google";
-
-import "./globals.css";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+import {dark} from '@clerk/themes'
 
 import { ThemeProvider } from "@/components/theme-provider";
+
+import "./globals.css";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -27,12 +36,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${inter.variable} ${plexMono.variable} antialiased`}
-      >
-        <ThemeProvider attribute={"class"} defaultTheme="dark" enableSystem disableTransitionOnChange>{children}</ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider appearance={{
+      theme: dark,
+    }}>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${inter.variable} ${plexMono.variable} antialiased`}
+        >
+          <ThemeProvider attribute={"class"} defaultTheme="dark" enableSystem disableTransitionOnChange>
+            <header>
+              <SignedOut><SignInButton /><SignUpButton><button className="bg-rose-500 text-white p-2 rounded">Sign up</button></SignUpButton></SignedOut> 
+              <SignedIn><UserButton /></SignedIn>
+            </header>
+            {children}</ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
